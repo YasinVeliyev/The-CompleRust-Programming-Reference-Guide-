d414468cb15b2808d7338b0258faa47799f0b554
YasinVeliyev <veliyev.yasin@gmail.com>
2022-11-06 20:56:18 +0400

    Strings

---
+++ Metaprogramming/into_map_demo/src/main.rs
@@ -0,0 +1,12 @@
+use into_map_derive::IntoMap;
+
+#[derive(IntoMap)]
+struct User{
+    name:String,
+    id:usize,
+    active:bool
+}
+fn main() {
+    let my_bar=User{name:"Alice".to_string(),id:35,active:false};
+    println!("Hello, world!");
+}
